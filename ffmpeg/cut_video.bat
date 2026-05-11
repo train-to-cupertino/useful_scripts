118c3c6fc358a557241279b707a8ecc3b6b5d51b
train-to-cupertino <49920253+train-to-cupertino@users.noreply.github.com>
2026-05-11 11:26:04 +0300

    Update cut_video.bat
    Adde "cut_x_seconds_from_end" functionality

--- ffmpeg/cut_video.bat
+++ ffmpeg/cut_video.bat
@@ -8,3 +8,21 @@ ffmpeg -sseof -02:12:07 -i input.mp4 -c copy output.mp4
 # input.mp4:  [00:00:00...01:00:00...02:00:00...03:00:00...04:00:00...04:35:33] (04:35:33 duration)
 # output.mp4: [00:00:00...01:00:00...02:00:00]                                  (02:00:00 duration)
 ffmpeg -i input.mp4 -ss 00:00:00 -to 02:00:00 -c copy output.mp4
+
+
+# Cut a part of the video from 00:00:00 to -X seconds from the end (cut away last X seconds of the video)
+# Example of usage: ./script.sh input.mp4 5 # 5 seconds from the end will be deleted
+input_file=$1
+cut_length=$2
+duration=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $input_file)
+end_time=$(echo "$duration - $cut_length" | bc)
+echo "Duration: " $duration
+echo "End time: " $end_time
+output_file=$(date +%s%3N)_$input_file
+command="ffmpeg -ss 0 -t $end_time -i $input_file -c copy $output_file"
+echo "Command: " $command
+$($command)
+#ffmpeg -i $input_file -ss 0 -t $end_time -c copy $(date +"%s%3N")_$input_file
+
+#ffmpeg -i b_The_model_in_the_pic.mp4 -ss 0 -t 8.042000 -c copy 1778487601037_b_The_model_in_the_pic.mp4
+#ffmpeg -ss 0 -t 8.042000 -i b_The_model_in_the_pic.mp4 -c copy b_copy2.mp4
